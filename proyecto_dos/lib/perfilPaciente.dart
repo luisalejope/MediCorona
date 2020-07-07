8f903ac8425a4cb181a0ac381b45fe302bef6276
josephrojas <36609457+josephrojas@users.noreply.github.com>
2020-07-07 10:23:01 -0500

    pantallas paciente

--- proyecto_dos/lib/perfilPaciente.dart
+++ proyecto_dos/lib/perfilPaciente.dart
@@ -1,272 +1,163 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
-class PerfilPaciente extends StatelessWidget {
-  PerfilPaciente({
-    Key key,
-  }) : super(key: key);
+class Paciente extends StatefulWidget {
+  @override
+  _PacienteState createState() => _PacienteState();
+}
+
+class _PacienteState extends State<Paciente> {
+  Persona personita = Persona('Armando', 'Gonzalez', 'Cedula de ciudadania',
+      '1026435', 'Cra 43 # 23', 'Chia', 'Sanitas', '3124325465');
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xffffffff),
-      body: Stack(
-        children: <Widget>[
-          Container(
-            width: 375.0,
-            height: 302.0,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.only(
-                bottomRight: Radius.circular(5.0),
-                bottomLeft: Radius.circular(5.0),
-              ),
-              color: const Color(0xff32a8ff),
-            ),
-          ),
-          Transform.translate(
-            offset: Offset(22.0, 151.0),
-            child: SizedBox(
-              width: 261.0,
-              height: 58.0,
-              child: SingleChildScrollView(
-                  child: Text(
-                'Nombre \n',
-                style: TextStyle(
-                  fontFamily: 'Roboto',
-                  fontSize: 48,
-                  color: const Color(0xffffffff),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.only(top: 60.0),
+          color: Colors.blue[300],
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Expanded(
+                child: Text(
+                  personita.getName() + '\n' + personita.getApellido(),
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                  ),
                 ),
-                textAlign: TextAlign.left,
-              )),
-            ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  padding: EdgeInsets.all(0.0),
+                  onPressed: null,
+                  child: Icon(
+                    Icons.settings,
+                    size: 35.0,
+                  ),
+                ),
+              ),
+            ],
           ),
-          Transform.translate(
-            offset: Offset(22.0, 224.0),
-            child: SizedBox(
-              width: 229.0,
-              height: 64.0,
+        ),
+        Row(
+          children: <Widget>[
+            Expanded(
               child: Text(
-                'Apellido',
-                style: TextStyle(
-                  fontFamily: 'Roboto',
-                  fontSize: 48,
-                  color: const Color(0xffffffff),
-                ),
-                textAlign: TextAlign.left,
+                'Tipo documento ',
+                style: TextStyle(fontWeight: FontWeight.bold),
               ),
             ),
-          ),
-          Transform.translate(
-            offset: Offset(12.0, 346.0),
-            child: SizedBox(
-              width: 176.0,
-              height: 41.0,
+            Expanded(
               child: Text(
-                'Tipo de Documento',
-                style: TextStyle(
-                  fontFamily: 'Roboto',
-                  fontSize: 18,
-                  color: const Color(0xff707070),
-                  fontWeight: FontWeight.w700,
-                ),
-                textAlign: TextAlign.left,
+                personita.gettipoDocumento(),
               ),
             ),
-          ),
-          Transform.translate(
-            offset: Offset(199.0, 346.0),
-            child: SizedBox(
-              width: 176.0,
-              height: 41.0,
+          ],
+        ),
+        Row(
+          children: <Widget>[
+            Expanded(
               child: Text(
-                'No. de Documento',
-                style: TextStyle(
-                  fontFamily: 'Roboto',
-                  fontSize: 18,
-                  color: const Color(0xff707070),
-                  fontWeight: FontWeight.w700,
-                ),
-                textAlign: TextAlign.left,
+                'Numero documento ',
+                style: TextStyle(fontWeight: FontWeight.bold),
               ),
             ),
-          ),
-          Transform.translate(
-            offset: Offset(12.0, 428.0),
-            child: SizedBox(
-              width: 176.0,
-              height: 41.0,
+            Expanded(
               child: Text(
-                'Dirección',
-                style: TextStyle(
-                  fontFamily: 'Roboto',
-                  fontSize: 18,
-                  color: const Color(0xff707070),
-                  fontWeight: FontWeight.w700,
-                ),
-                textAlign: TextAlign.left,
+                personita.getIdNumber(),
               ),
             ),
-          ),
-          Transform.translate(
-            offset: Offset(199.0, 428.0),
-            child: SizedBox(
-              width: 176.0,
-              height: 41.0,
+          ],
+        ),
+        Row(
+          children: <Widget>[
+            Expanded(
+              child: Text(
+                'Direccion',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+            Expanded(
+              child: Text(
+                personita.getDireccion(),
+              ),
+            ),
+          ],
+        ),
+        Row(
+          children: <Widget>[
+            Expanded(
               child: Text(
                 'Ciudad',
-                style: TextStyle(
-                  fontFamily: 'Roboto',
-                  fontSize: 18,
-                  color: const Color(0xff707070),
-                  fontWeight: FontWeight.w700,
-                ),
-                textAlign: TextAlign.left,
+                style: TextStyle(fontWeight: FontWeight.bold),
               ),
             ),
-          ),
-          Transform.translate(
-            offset: Offset(12.0, 510.0),
-            child: SizedBox(
-              width: 176.0,
-              height: 41.0,
+            Expanded(
+              child: Text(
+                personita.getCiudad(),
+              ),
+            ),
+          ],
+        ),
+        Row(
+          children: <Widget>[
+            Expanded(
               child: Text(
                 'EPS',
-                style: TextStyle(
-                  fontFamily: 'Roboto',
-                  fontSize: 18,
-                  color: const Color(0xff707070),
-                  fontWeight: FontWeight.w700,
-                ),
-                textAlign: TextAlign.left,
+                style: TextStyle(fontWeight: FontWeight.bold),
               ),
             ),
-          ),
-          Transform.translate(
-            offset: Offset(199.0, 510.0),
-            child: SizedBox(
-              width: 176.0,
-              height: 41.0,
+            Expanded(
+              child: Text(
+                personita.getEps(),
+              ),
+            ),
+          ],
+        ),
+        Row(
+          children: <Widget>[
+            Expanded(
               child: Text(
                 'Celular',
-                style: TextStyle(
-                  fontFamily: 'Roboto',
-                  fontSize: 18,
-                  color: const Color(0xff707070),
-                  fontWeight: FontWeight.w700,
-                ),
-                textAlign: TextAlign.left,
+                style: TextStyle(fontWeight: FontWeight.bold),
               ),
             ),
-          ),
-          Transform.translate(
-            offset: Offset(12.0, 387.0),
-            child: SizedBox(
-              width: 176.0,
-              height: 41.0,
+            Expanded(
               child: Text(
-                'C.C.',
-                style: TextStyle(
-                  fontFamily: 'Roboto',
-                  fontSize: 18,
-                  color: const Color(0xff707070),
-                ),
-                textAlign: TextAlign.left,
+                personita.getCelular(),
               ),
             ),
-          ),
-          Transform.translate(
-            offset: Offset(199.0, 387.0),
-            child: SizedBox(
-              width: 176.0,
-              height: 41.0,
-              child: Text(
-                '102223316',
-                style: TextStyle(
-                  fontFamily: 'Roboto',
-                  fontSize: 18,
-                  color: const Color(0xff707070),
-                ),
-                textAlign: TextAlign.left,
-              ),
-            ),
-          ),
-          Transform.translate(
-            offset: Offset(12.0, 469.0),
-            child: SizedBox(
-              width: 176.0,
-              height: 41.0,
-              child: Text(
-                'Av. Pradilla',
-                style: TextStyle(
-                  fontFamily: 'Roboto',
-                  fontSize: 18,
-                  color: const Color(0xff707070),
-                ),
-                textAlign: TextAlign.left,
-              ),
-            ),
-          ),
-          Transform.translate(
-            offset: Offset(199.0, 469.0),
-            child: SizedBox(
-              width: 176.0,
-              height: 41.0,
-              child: Text(
-                'Chía',
-                style: TextStyle(
-                  fontFamily: 'Roboto',
-                  fontSize: 18,
-                  color: const Color(0xff707070),
-                ),
-                textAlign: TextAlign.left,
-              ),
-            ),
-          ),
-          Transform.translate(
-            offset: Offset(12.0, 551.0),
-            child: SizedBox(
-              width: 176.0,
-              height: 41.0,
-              child: Text(
-                'Sanitas',
-                style: TextStyle(
-                  fontFamily: 'Roboto',
-                  fontSize: 18,
-                  color: const Color(0xff707070),
-                ),
-                textAlign: TextAlign.left,
-              ),
-            ),
-          ),
-          Transform.translate(
-            offset: Offset(199.0, 551.0),
-            child: SizedBox(
-              width: 176.0,
-              height: 41.0,
-              child: Text(
-                '31088887775',
-                style: TextStyle(
-                  fontFamily: 'Roboto',
-                  fontSize: 18,
-                  color: const Color(0xff707070),
-                ),
-                textAlign: TextAlign.left,
-              ),
-            ),
-          ),
-          Transform.translate(
-            offset: Offset(323.62, 248.0),
-            child:
-                // Adobe XD layer: 'ic_brightness_low_2…' (shape)
-                SvgPicture.string(
-              _svg_qj7na,
-              allowDrawingOutsideViewBox: true,
-            ),
-          ),
-        ],
-      ),
+          ],
+        ),
+        Container(
+          width: 40.0,
+        ),
+      ],
     );
   }
 }
 
-const String _svg_qj7na =
-    '<svg viewBox="323.6 248.0 40.0 40.0" ><path transform="translate(322.93, 247.31)" d="M 34.83672332763672 26.54319190979004 L 40.68994140625 20.68997192382813 L 34.83672332763672 14.83675193786621 L 34.83672332763672 6.543218612670898 L 26.54319000244141 6.543218612670898 L 20.68997192382813 0.6899999380111694 L 14.83675193786621 6.543218612670898 L 6.543218612670898 6.543218612670898 L 6.543218612670898 14.83675384521484 L 0.6899999380111694 20.68997192382813 L 6.543218612670898 26.54319190979004 L 6.543218612670898 34.83672332763672 L 14.83675384521484 34.83672332763672 L 20.68997192382813 40.68994140625 L 26.54319190979004 34.83672332763672 L 34.83672332763672 34.83672332763672 L 34.83672332763672 26.54319000244141 Z M 20.68997192382813 31.30003547668457 C 14.83675384521484 31.30003547668457 10.07990741729736 26.54319000244141 10.07990741729736 20.68997192382813 C 10.07990741729736 14.83675384521484 14.83675384521484 10.07990741729736 20.68997192382813 10.07990741729736 C 26.54319000244141 10.07990741729736 31.30003547668457 14.83675384521484 31.30003547668457 20.68997192382813 C 31.30003547668457 26.54319000244141 26.54319000244141 31.30003547668457 20.68997192382813 31.30003547668457 Z" fill="#ffffff" stroke="none" stroke-width="1" stroke-miterlimit="4" stroke-linecap="butt" /></svg>';
+class Persona {
+  String _name = '';
+  String _apellido = '';
+  String _tipoDocumento = '';
+  String _idNUmber = '';
+  String _direccion = '';
+  String _ciudad = '';
+  String _eps = '';
+  String _celular = '';
+
+  Persona(this._name, this._apellido, this._tipoDocumento, this._idNUmber,
+      this._direccion, this._ciudad, this._eps, this._celular);
+
+  String getName() => this._name;
+  String getApellido() => this._apellido;
+  String gettipoDocumento() => this._tipoDocumento;
+  String getIdNumber() => this._idNUmber;
+  String getDireccion() => this._direccion;
+  String getCiudad() => this._ciudad;
+  String getEps() => this._eps;
+  String getCelular() => this._celular;
+}
--- proyecto_dos/lib/revisarSolicitud.dart
+++ proyecto_dos/lib/revisarSolicitud.dart
@@ -1,215 +1,127 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
-class RevisarSolicitud extends StatelessWidget {
-  RevisarSolicitud({
-    Key key,
-  }) : super(key: key);
+class RevisarSolicitud extends StatefulWidget {
+  @override
+  _RevisarSolicitudState createState() => _RevisarSolicitudState();
+}
+
+class _RevisarSolicitudState extends State<RevisarSolicitud> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xffffffff),
-      body: Stack(
-        children: <Widget>[
-          Container(
-            width: 375.0,
-            height: 78.0,
-            decoration: BoxDecoration(
-              border: Border.all(width: 1.0, color: const Color(0xff707070)),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Center(
+          child: Text(
+            'Nombre medico',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
+          ),
+        ),
+        Center(
+          child: Icon(
+            Icons.accessibility,
+            size: 40.0,
+            color: Colors.grey,
+          ),
+        ),
+        SizedBox(
+          height: 20.0, // match_parent
+        ),
+        Text(
+          'Hora de solicitud',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          '9:35 - variable',
+          style: TextStyle(
+            color: Colors.blue[300],
+          ),
+        ),
+        Divider(
+          color: Colors.grey,
+          thickness: 2,
+          height: 25,
+        ),
+        Text(
+          'Día',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          'fecha - variable',
+          style: TextStyle(
+            color: Colors.blue[300],
+          ),
+        ),
+        Divider(
+          color: Colors.grey,
+          thickness: 2,
+          height: 25,
+        ),
+        Text(
+          'Celular',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          'numero - variable',
+          style: TextStyle(
+            color: Colors.blue[300],
+          ),
+        ),
+        Divider(
+          color: Colors.grey,
+          thickness: 2,
+          height: 25,
+        ),
+        Text(
+          'Disponibilidad',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          'variable',
+          style: TextStyle(
+            color: Colors.blue[300],
+          ),
+        ),
+        Divider(
+          color: Colors.grey,
+          thickness: 2,
+          height: 25,
+        ),
+        Text(
+          'Descripción',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          'Descripcion variable',
+          style: TextStyle(
+            color: Colors.blue[300],
+          ),
+        ),
+        SizedBox(
+          height: 20.0, // match_parent
+        ),
+        Center(
+          child: ButtonTheme(
+            minWidth: 150.0,
+            height: 40.0,
+            child: RaisedButton(
+              onPressed: (() {}),
+              color: Colors.red,
+              child: Text('Cancelar', style: TextStyle(fontSize: 20)),
             ),
           ),
-          Transform.translate(
-            offset: Offset(91.0, 113.0),
-            child: SizedBox(
-              width: 193.0,
-              height: 29.0,
-              child: SingleChildScrollView(
-                  child: Text(
-                'Nombre Médico\n',
-                style: TextStyle(
-                  fontFamily: 'Roboto',
-                  fontSize: 24,
-                  color: const Color(0xff000000),
-                  fontWeight: FontWeight.w700,
-                ),
-                textAlign: TextAlign.center,
-              )),
-            ),
-          ),
-          Transform.translate(
-            offset: Offset(22.0, 244.0),
-            child: SizedBox(
-              width: 267.0,
-              height: 41.0,
-              child: Text(
-                'Horario:',
-                style: TextStyle(
-                  fontFamily: 'Roboto',
-                  fontSize: 18,
-                  color: const Color(0xff000000),
-                  fontWeight: FontWeight.w700,
-                ),
-                textAlign: TextAlign.left,
-              ),
-            ),
-          ),
-          Transform.translate(
-            offset: Offset(22.0, 326.0),
-            child: SizedBox(
-              width: 267.0,
-              height: 41.0,
-              child: Text(
-                'Fecha:',
-                style: TextStyle(
-                  fontFamily: 'Roboto',
-                  fontSize: 18,
-                  color: const Color(0xff000000),
-                  fontWeight: FontWeight.w700,
-                ),
-                textAlign: TextAlign.left,
-              ),
-            ),
-          ),
-          Transform.translate(
-            offset: Offset(22.0, 408.0),
-            child: SizedBox(
-              width: 267.0,
-              height: 41.0,
-              child: Text(
-                'Descripción:',
-                style: TextStyle(
-                  fontFamily: 'Roboto',
-                  fontSize: 18,
-                  color: const Color(0xff000000),
-                  fontWeight: FontWeight.w700,
-                ),
-                textAlign: TextAlign.left,
-              ),
-            ),
-          ),
-          Transform.translate(
-            offset: Offset(22.0, 275.0),
-            child: SizedBox(
-              width: 267.0,
-              height: 41.0,
-              child: Text(
-                'Tarde',
-                style: TextStyle(
-                  fontFamily: 'Roboto',
-                  fontSize: 18,
-                  color: const Color(0xff32a8ff),
-                ),
-                textAlign: TextAlign.left,
-              ),
-            ),
-          ),
-          Transform.translate(
-            offset: Offset(22.0, 363.0),
-            child: SizedBox(
-              width: 267.0,
-              height: 41.0,
-              child: Text(
-                '20/07/2020',
-                style: TextStyle(
-                  fontFamily: 'Roboto',
-                  fontSize: 18,
-                  color: const Color(0xff32a8ff),
-                ),
-                textAlign: TextAlign.left,
-              ),
-            ),
-          ),
-          Transform.translate(
-            offset: Offset(22.0, 444.0),
-            child: SizedBox(
-              width: 267.0,
-              height: 41.0,
-              child: Text(
-                'Dolor de cabeza fuerte.',
-                style: TextStyle(
-                  fontFamily: 'Roboto',
-                  fontSize: 18,
-                  color: const Color(0xff32a8ff),
-                ),
-                textAlign: TextAlign.left,
-              ),
-            ),
-          ),
-          Transform.translate(
-            offset: Offset(81.0, 26.0),
-            child: SizedBox(
-              width: 214.0,
-              height: 26.0,
-              child: Text(
-                'Información de Solicitud',
-                style: TextStyle(
-                  fontFamily: 'Roboto',
-                  fontSize: 18,
-                  color: const Color(0xff000000),
-                ),
-                textAlign: TextAlign.center,
-              ),
-            ),
-          ),
-          Transform.translate(
-            offset: Offset(12.0, 23.04),
-            child:
-                // Adobe XD layer: 'ic_keyboard_arrow_l…' (shape)
-                SvgPicture.string(
-              _svg_oroev3,
-              allowDrawingOutsideViewBox: true,
-            ),
-          ),
-          Transform.translate(
-            offset: Offset(12.0, 572.0),
-            child: Container(
-              width: 351.0,
-              height: 61.0,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(5.0),
-                color: const Color(0xffd20000),
-                boxShadow: [
-                  BoxShadow(
-                    color: const Color(0x29000000),
-                    offset: Offset(4, 4),
-                    blurRadius: 6,
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Transform.translate(
-            offset: Offset(80.0, 588.0),
-            child: SizedBox(
-              width: 215.0,
-              height: 29.0,
-              child: SingleChildScrollView(
-                  child: Text(
-                'Cancelar Solicitud\n',
-                style: TextStyle(
-                  fontFamily: 'Roboto',
-                  fontSize: 24,
-                  color: const Color(0xffffffff),
-                  fontWeight: FontWeight.w700,
-                ),
-                textAlign: TextAlign.center,
-              )),
-            ),
-          ),
-          Transform.translate(
-            offset: Offset(21.5, 315.5),
-            child: SvgPicture.string(
-              _svg_jr9324,
-              allowDrawingOutsideViewBox: true,
-            ),
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
-
-const String _svg_oroev3 =
-    '<svg viewBox="12.0 23.0 198.0 174.0" ><path transform="translate(4.0, 17.54)" d="M 27.11903381347656 32.82396697998047 L 15.30187129974365 20.98100090026855 L 27.11903381347656 9.138034820556641 L 23.48100280761719 5.5 L 7.999999046325684 20.98100090026855 L 23.48100280761719 36.46200180053711 L 27.11903381347656 32.82396697998047 Z" fill="#32a8ff" stroke="none" stroke-width="1" stroke-miterlimit="4" stroke-linecap="butt" /><path transform="translate(160.13, 147.13)" d="M 26.93284606933594 26.93284606933594 C 33.26804351806641 26.93284606933594 38.39926910400391 21.80162239074707 38.39926910400391 15.46642303466797 C 38.39926910400391 9.131223678588867 33.26804351806641 4 26.93284606933594 4 C 20.59764671325684 4 15.46642303466797 9.131223678588867 15.46642303466797 15.46642303466797 C 15.46642303466797 21.80162239074707 20.59764671325684 26.93284606933594 26.93284606933594 26.93284606933594 Z M 26.93284606933594 32.66605758666992 C 19.27900886535645 32.66605758666992 4 36.50730895996094 4 44.13248062133789 L 4 49.86569213867188 L 49.86569213867188 49.86569213867188 L 49.86569213867188 44.13248062133789 C 49.86569213867188 36.50730895996094 34.58668518066406 32.66605758666992 26.93284606933594 32.66605758666992 Z" fill="#000000" stroke="none" stroke-width="1" stroke-miterlimit="4" stroke-linecap="butt" /></svg>';
-const String _svg_jr9324 =
-    '<svg viewBox="21.5 315.5 333.0 162.0" ><path transform="translate(21.5, 315.5)" d="M 0 0 L 333 0" fill="none" stroke="#707070" stroke-width="1" stroke-miterlimit="4" stroke-linecap="butt" /><path transform="translate(21.5, 396.5)" d="M 0 0 L 333 0" fill="none" stroke="#707070" stroke-width="1" stroke-miterlimit="4" stroke-linecap="butt" /><path transform="translate(21.5, 477.5)" d="M 0 0 L 333 0" fill="none" stroke="#707070" stroke-width="1" stroke-miterlimit="4" stroke-linecap="butt" /></svg>';
