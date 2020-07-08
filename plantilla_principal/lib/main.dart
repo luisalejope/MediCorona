9c3cc837b271c60f345e7aab32e0f9087c8ad648
josephrojas <36609457+josephrojas@users.noreply.github.com>
2020-07-07 20:34:50 -0500

    cambios nuevo perfil

--- plantilla_principal/lib/main.dart
+++ plantilla_principal/lib/main.dart
@@ -3,9 +3,15 @@ import 'package:flutter/material.dart';
 import 'newPerfil.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: NewUser(),
-    ),
-  );
+    );
+  }
 }
